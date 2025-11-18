cc5a5956cba4a9629092dc5084cc3818a7238ffa
Ubuntu <ubuntu@ip-172-31-31-237.eu-central-1.compute.internal>
2025-11-18 17:57:55 +0000

    bug

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,12 +3,15 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from Spring Boooooot!";
+    @GetMapping("/divide")
+    public int sayHello(@RequestParam int by) {
+        int dividend = by - 1;
+        int result = 10/dividend;
+        return result;
     }
 }
